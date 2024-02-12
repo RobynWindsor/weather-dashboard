58eff04458be15723da28178ce133510ebb6984b
Robyn <127926809+Robyn-web@users.noreply.github.com>
2024-02-12 17:47:03 +0000

    forecast html added

--- src/script.js
+++ src/script.js
@@ -26,3 +26,39 @@ function displayForecast(response) {
   console.log(response);
   let forecast = response.data.daily.slice(1);
   // Skip first day
+
+  let forecastElement = document.querySelector('#forecast');
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (day) {
+    const timeSeconds = day.time;
+    const timeMilliseconds = timeSeconds * 1000;
+    const dayIndexOfWeek = new Date(timeMilliseconds).getDay();
+    const dayNameOfWeek = days[dayIndexOfWeek];
+
+    console.log(day);
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2 weather-forecast">
+        <div class="weather-forecast-date">${dayNameOfWeek}</div>
+        <img
+          src="${day.condition.icon_url}"
+          alt="${day.condition.description}"
+          title="${day.condition.description}"
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            day.temperature.maximum
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            day.temperature.minimum
+          )}° </span>
+        </div>
+      </div>
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
